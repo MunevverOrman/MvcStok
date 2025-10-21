16116d45b2744ebd1ba611ec01386c2b840f4cbe
Münevver Orman <munevverorman1@gmail.com>
2025-10-21 22:24:14 +0300

    popup modal kullanımı,paging&seach

---
+++ MvcStok/Controllers/SalesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class SalesController : Controller
+    {
+        // GET: Sales
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult CreateSale()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateSale(TBLSATISLAR p)
+        {
+            db.TBLSATISLAR.Add(p);
+            db.SaveChanges();
+            return View("Index");
+        }
+    }
+}
